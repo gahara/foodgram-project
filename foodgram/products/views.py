825f9372e0f78414017a552ef48a184cef456ab4
Madina Alieva <gahara@yandex-team.ru>
2020-11-17 14:39:58 +0300

    review 2

--- foodgram/products/views.py
+++ foodgram/products/views.py
@@ -13,14 +13,6 @@ from .models import Content, Favourite, Ingredient, Recipe, ShopList, Tag
 from .utils import get_ingredients, get_paginator, get_tags_for_edit, DEFAULT_TAGS
 
 
-def page_not_found(request, exception):
-    return render(request, 'misc/404.html', {'path': request.path}, status=404)
-
-
-def server_error(request):
-    return render(request, 'misc/500.html', status=500)
-
-
 def list_ingredients(request):
     text = request.GET['query']
 
